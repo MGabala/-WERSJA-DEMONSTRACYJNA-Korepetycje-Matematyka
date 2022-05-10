943784338d18865923aa22254bd666778c028bde
M.Gabała <82496006+MGabala@users.noreply.github.com>
2022-05-10 15:20:24 +0200

    update

---
+++ Data/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace Korepetycje_Matematyka.Data
+{
+    public class Account
+    {
+        public int Id { get; set; } 
+        public string? Login { get; set; }
+        public string? Password { get; set; }
+        public long PhoneNumber { get; set; }
+    }
+}
