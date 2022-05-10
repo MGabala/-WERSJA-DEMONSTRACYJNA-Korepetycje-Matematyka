92ce1142c940f8658bbee5cfbc27e8e11a0135b9
M.Gabała <82496006+MGabala@users.noreply.github.com>
2022-05-10 16:11:57 +0200

    Setting Model Binding

--- Pages/AddUser.cshtml.cs
+++ Pages/AddUser.cshtml.cs
@@ -6,11 +6,12 @@ namespace Korepetycje_Matematyka.Pages
 {
     public class AddUserModel : PageModel
     {
+        [BindProperty]
         public Account NewUser { get; set; }
          
         public void OnPost()
         {
-
+            var newUser = NewUser.Login;
         }
     }
 }
