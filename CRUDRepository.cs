1a22afa7d46c25437d6d62c83e2c119f5fec48c0
M.Gabała <82496006+MGabala@users.noreply.github.com>
2022-05-12 12:03:51 +0200

    update

---
+++ CRUDRepository.cs
@@ -0,0 +1,35 @@
+﻿namespace Korepetycje_Matematyka
+{
+    public class CRUDRepository : ICRUDRepository
+    {
+        public Task CreateAsync(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Account>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Account?> GetOneAsync(int phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserExistsAsync(int phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ICRUDRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Korepetycje_Matematyka
+{
+    public interface ICRUDRepository
+    {
+        public Task<IEnumerable<Account>> GetAllAsync();
+        public Task<Account?> GetOneAsync(int phoneNumber);
+        Task<bool> UserExistsAsync(int phoneNumber);
+        Task<bool> SaveChangesAsync();
+        Task DeleteAsync(int phoneNumber);
+        Task CreateAsync(Account account);
+    }
+}
