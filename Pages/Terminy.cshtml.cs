ce33166c77b8e48676b3e56e1fd1cabe8f393532
M.Gabała <82496006+MGabala@users.noreply.github.com>
2022-05-13 18:22:52 +0200

    update

--- Pages/Terminy.cshtml.cs
+++ Pages/Terminy.cshtml.cs
@@ -1,10 +1,10 @@
-
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Korepetycje_Matematyka.Pages
 {
     public class TerminyModel : PageModel
     {
-      
         public void OnGet()
         {
         }
