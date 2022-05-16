158b4dc28f70ba72c42ee847c5a36e73cc471972
M.Gabała <82496006+MGabala@users.noreply.github.com>
2022-05-16 12:37:14 +0200

    update

--- Pages/CreateTerminy.cshtml.cs
+++ Pages/CreateTerminy.cshtml.cs
@@ -5,6 +5,14 @@ namespace Korepetycje_Matematyka.Pages
 {
     public class CreateTerminyModel : PageModel
     {
+        private ICRUDRepository repo;
+        [FromRoute]
+        public int Id { get; set; }
+        public Terminy Terminy { get; set; }
+        public CreateTerminyModel(ICRUDRepository repository)
+        {
+            this.repo = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
         public void OnGet()
         {
         }
