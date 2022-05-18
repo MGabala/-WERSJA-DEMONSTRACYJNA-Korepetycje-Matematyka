2b634a37ce0bfa011d334ac3fdffa1b6b1859fa7
M.Gabała <82496006+MGabala@users.noreply.github.com>
2022-05-18 16:25:23 +0200

    Authorization and Authentication works

--- Pages/ADMIN_ListaTerminów.cshtml.cs
+++ Pages/ADMIN_ListaTerminów.cshtml.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Korepetycje_Matematyka.Pages
 {
+    [Authorize(Roles = "Admins")]
     public class ADMIN_ListaTerminówModel : PageModel
     {
         private ITerminyRepository repo;
