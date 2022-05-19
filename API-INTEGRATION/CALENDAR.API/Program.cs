6ed4b9e4c8a2f0b1ad67abddd931af7c5c21eab8
M.Gabała <82496006+MGabala@users.noreply.github.com>
2022-05-19 16:14:41 +0200

    Update

--- API-INTEGRATION/CALENDAR.API/Program.cs
+++ API-INTEGRATION/CALENDAR.API/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿
+using System;
 
 namespace CALENDAR_INTEGRATION // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        static void Main(string[] args)
+        const string ApiKey_ = "AIzaSyAxriXYldEFvas2zCfdUgXzZCpohez2xd4";
+        const string CalendarId = "en.danish#holiday@group.v.calendar.google.com";
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var service = new Google.Apis.Calendar.v3.CalendarService(new Google.Apis.Services.BaseClientService.Initializer()
+            {
+                ApiKey = ApiKey_,
+                ApplicationName = "Test"
+            });
+            var request = service.Events.List(CalendarId);
+            request.Fields = "items(summary,start,end)";
+            var response = await request.ExecuteAsync();
+            foreach(var item in response.Items)
+            {
+                Console.WriteLine($"Holiday: {item.Summary} start: {item.Start} end: {item.End}");
+            }
+            Console.ReadLine();
         }
+      
     }
 }
