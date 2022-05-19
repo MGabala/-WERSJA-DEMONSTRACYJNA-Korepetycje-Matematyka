e28b8f46b6a6956781d71ad9ec67d28ee67a74ce
M.Gabała <82496006+MGabala@users.noreply.github.com>
2022-05-19 10:48:49 +0200

    Account Manager + Terminy działają. Mechanizm zapisywania się na korepetycje + potwierdzenia in progress

--- Program.cs
+++ Program.cs
@@ -28,20 +28,18 @@ builder.Services.Configure<CookieAuthenticationOptions>(
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
 
 app.UseHttpsRedirection();
-
+app.UseHsts();
 //HttpLogging -> show logs in console
 app.UseHttpLogging();
-
 app.UseStaticFiles();
-
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
