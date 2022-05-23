408fcd84cfe77a7c960bc7ffad66f95aff933a79
M.Gabała <82496006+MGabala@users.noreply.github.com>
2022-05-23 18:53:30 +0200

    update

--- Pages/Terminy.cshtml.cs
+++ Pages/Terminy.cshtml.cs
@@ -1,16 +1,10 @@
-
-using DotNetOpenAuth.InfoCard;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Net;
 using System.Net.Mail;
-using System.Security.Claims;
 
 namespace Korepetycje_Matematyka.Pages
 {
-  //  [Authorize(Roles = "User")]
+    //  [Authorize(Roles = "User")]
 
     public class TerminyModel : PageModel
     {
@@ -19,7 +13,6 @@ namespace Korepetycje_Matematyka.Pages
         private readonly string _mailFrom = string.Empty;
         public UserManager<IdentityUser> UserManager;
         public IEnumerable<Terminy> Terminy { get; set; }
-
         public TerminyModel(ITerminyRepository repository, IConfiguration configuration, UserManager<IdentityUser> usrManager)
         {
             UserManager = usrManager;
@@ -30,22 +23,19 @@ namespace Korepetycje_Matematyka.Pages
         }
         public async Task OnGet()
         {
-            //Accounts = await AccountsRepo.GetAllAsync();
             Terminy = await repo.GetAllTerminyAsync();
-            var userId = this.User.FindFirstValue(ClaimTypes.Name);
         }
         public async Task<IActionResult> OnPostAsync()
         {
-            string ID = User.Identity.Name;
+            var user = await UserManager.GetUserAsync(User);
             using (MailMessage mail = new MailMessage())
             using (SmtpClient smtpClient = new SmtpClient())
             {
-
                 smtpClient.Host = "smtp-mail.outlook.com";
                 smtpClient.Port = 587;
                 smtpClient.Credentials = new NetworkCredential(_mailFrom, Environment.GetEnvironmentVariable("HAS£O"));
                 smtpClient.EnableSsl = true;
-                mail.Body = $"{ID} zapisa³ siê na termin";
+                mail.Body = $"U¿ytkownik: {user.UserName} o numerze: {user.PhoneNumber} zapisa³ siê na termin..";
                 mail.IsBodyHtml = false;
                 mail.From = new MailAddress(_mailFrom, "TYTU£ ");
                 mail.To.Add(_mailTo);
