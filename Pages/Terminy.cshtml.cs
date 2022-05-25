127f8003f9016b4f95206e0d2f1e8e71eb89d276
M.Gabała <82496006+MGabala@users.noreply.github.com>
2022-05-25 09:56:33 +0200

    update

--- Pages/Terminy.cshtml.cs
+++ Pages/Terminy.cshtml.cs
@@ -12,21 +12,28 @@ namespace Korepetycje_Matematyka.Pages
         private readonly string _mailTo = string.Empty;
         private readonly string _mailFrom = string.Empty;
         public UserManager<IdentityUser> UserManager;
+     
+        public string DzieñTygodnia { get; set; }
         public IEnumerable<Terminy> Terminy { get; set; }
+       
+        
         public TerminyModel(ITerminyRepository repository, IConfiguration configuration, UserManager<IdentityUser> usrManager)
         {
             UserManager = usrManager;
             this.repo = repository;
             _mailTo = configuration["mailSettings:mailTo"];
             _mailFrom = configuration["mailSettings:mailFrom"];
-          
+           
         }
         public async Task OnGet()
         {
             Terminy = await repo.GetAllTerminyAsync();
+            
         } 
         public async Task<IActionResult> OnPostAsync()
         {
+           
+            var request = Request.Form["name"];
             var user = await UserManager.GetUserAsync(User);
             using (MailMessage mail = new MailMessage())
             using (SmtpClient smtpClient = new SmtpClient())
@@ -35,7 +42,7 @@ namespace Korepetycje_Matematyka.Pages
                 smtpClient.Port = 587;
                 smtpClient.Credentials = new NetworkCredential(_mailFrom, Environment.GetEnvironmentVariable("HAS£O"));
                 smtpClient.EnableSsl = true;
-                mail.Body = $"U¿ytkownik: {user.UserName} o numerze: {user.PhoneNumber} zapisa³ siê na termin..";
+                mail.Body = $"U¿ytkownik: {user.UserName} o numerze: {user.PhoneNumber} zapisa³ siê na termin ";
                 mail.IsBodyHtml = false;
                 mail.From = new MailAddress(_mailFrom, "TYTU£ ");
                 mail.To.Add(_mailTo);
